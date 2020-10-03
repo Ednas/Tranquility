70086c4187ef117d1537bfac6df3c9a8ab9a1360
ednas <edna@edna.tech>
2020-10-03 11:26:52 -0400

    Creating first component, update styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
+import Home from './Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Home />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+export default function() {
+  return (
+    <div className="home">
+      <h1 style={{ textAlign: "center" }}>NASA Earth</h1>
+      <p>Introduction to NASA's Earth Science</p>
+      <p>NASA’s activities in space have brought new knowledge of the Earth, inspiring new ways of thinking about humanity and the planet. However, many people aren’t aware that NASA studies the Earth in addition to other planets. Your challenge is to tell stories of NASA’s Earth science enterprise using interactive digital tools. This will test your technical skills and your ability to think like a historian or educator.</p>
+      <p>Learn what NASA has to offer in this interactive tutorial</p>
+      <p>You can do it!</p>
+    </div>
+  )
+}

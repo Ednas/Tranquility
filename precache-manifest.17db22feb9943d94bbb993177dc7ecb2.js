dce8dfcc4cb5615bcb6026f4ed0f9c297e644545
ednas <edna@edna.tech>
2020-10-04 13:39:09 -0400

    Updates

---
+++ precache-manifest.17db22feb9943d94bbb993177dc7ecb2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "996a8fe815e7695cb00d436ebbd10cdb",
+    "url": "/Tranquility/index.html"
+  },
+  {
+    "revision": "a69f73859801340e5981",
+    "url": "/Tranquility/static/css/2.3d6ebafa.chunk.css"
+  },
+  {
+    "revision": "578a5e36efd3557d3a51",
+    "url": "/Tranquility/static/css/main.61f06d5f.chunk.css"
+  },
+  {
+    "revision": "a69f73859801340e5981",
+    "url": "/Tranquility/static/js/2.d3d48535.chunk.js"
+  },
+  {
+    "revision": "92bcffc488d6f3fbd12a7720ee4a3b28",
+    "url": "/Tranquility/static/js/2.d3d48535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "578a5e36efd3557d3a51",
+    "url": "/Tranquility/static/js/main.8fc20a36.chunk.js"
+  },
+  {
+    "revision": "d230fc027c0f68d24428",
+    "url": "/Tranquility/static/js/runtime-main.6dc52158.js"
+  },
+  {
+    "revision": "06018f0ee2f0247448bd82d440d71921",
+    "url": "/Tranquility/static/media/globe.06018f0e.svg"
+  }
+]);
